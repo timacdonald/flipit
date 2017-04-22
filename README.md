984fbc68b06d0e326a96cca66f7bb8806d42145c
Tim MacDonald <tim.mac7@me.com>
2017-04-22 14:52:27 +1000

    removed code comments better docs

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# FlipIt
+
 A simple script to SSH into your server and swap out your website with a fresh git repo version with zero downtime.
 
 Why? See my post: [Writing a Zero Downtime Deployment Script](https://timacdonald.me/writing-a-zero-downtime-deployment-script/).
+
+# Installation
+
+Git clone the repository onto your local machine and you can run the script from there.
+
+```
+git clone https://github.com/timacdonald/flipit.git
+
+## Usage
+
+```
+# run the script (replace the arguments)
+./flipit.sh -c SSH_CONNECTION_DETAILS -s SERVER_PUBLIC_FOLDER -r REPOSITORY_URL -g REPOSITORY_PUBLIC_FOLDER
+```
+
+## Contributing
+
+Please feel free to suggest new ideas or send through pull requests to make this better. If you'd like to discuss the project, feel free to reach out on [Twitter](https://twitter.com/timacdonald87).
+
+## License
+
+This package is under the MIT License. See [LICENSE](https://github.com/timacdonald/flipit/blob/master/LICENSE.txt) file for details.
--- flipit.sh
+++ flipit.sh
@@ -1,63 +1,28 @@
 #!/bin/sh
 
-# Process our arguments.
-# -r GIT_REPOSITORY
-# -g REPOSITORY_PUBLIC_DIRECTORY_PATH
-# -s SERVER_PUBLIC_DIRECTORY_PATH
-# -c CONNECT_TO
 while getopts r:g:s:c: option
 do
     case "${option}"
     in
-        # Create our repository variable.
         r) GIT_REPOSITORY=${OPTARG};;
-
-        # Create my repository 'public' directory variable.
         g) REPOSITORY_PUBLIC_DIRECTORY_PATH=${OPTARG};;
-
-        # Create my servers 'public' directory variable.
         s) SERVER_PUBLIC_DIRECTORY_PATH=./${OPTARG};;
-
-        # SSH connection details
         c) CONNECT_TO=${OPTARG};;
     esac
 done
 
-# Create a variable to store the unix time.
-# As 'date +%s' itself is a command, we need to tell the system to interpret it
-#   as a command and not simply a string. To do this we can wrap the command in
-#   parenthesis and prepend it with the '$' sign, e.g. $(COMMAND_HERE).
 UNIX_TIME=$(date +%s)
 
-# Create a variable and store the path to this new deployment, we'll need it
-#   later.
 DEPLOYMENT_DIRECTORY=./flipit/deploys/$UNIX_TIME
 
-# SSH into the server
-# The -tt flag will mean that the scripts output will be returned to our console
-#   so we can see if it was successful, otherwise we don't get the console output.
-# The script provided in the heredoc will be executed on the remote server
 ssh -tt $CONNECT_TO <<REMOTE_SCRIPT
 
-# Make the directory. To learn more type 'man mkdir'.
-# The -p flag tells the system to also create any intermediate directories.
-# To utilise our 'DEPLOYMENT_DIRECTORY' variable we append the '$' sign.
 mkdir -p $DEPLOYMENT_DIRECTORY
 
-# Clone our respository and pass the deployment directory as the destination
-#   in which to store the repository.
 git clone --depth 1 $GIT_REPOSITORY $DEPLOYMENT_DIRECTORY
 
-# Make a link...oh I mean FlipIt™
-# The -s flag tells the system to make a [symbolic link](https://en.wikipedia.org/wiki/Symbolic_link). This is important
-#   and also very handy for a lot of things. If you've ever used [Laravel Valet](https://laravel.com/docs/5.4/valet)
-#   before, you've been creating symbolic links! Well worth investigating.
-# The -h flag tells the system not to follow any symbolic links. This was doing
-#   my head in I ended up with ./public_html/public_html links! Inception!
-# the -f flag tells the system that if a link already exists, remove it and add
-#   this link instead.
 ln -s -n -f $DEPLOYMENT_DIRECTORY/$REPOSITORY_PUBLIC_DIRECTORY_PATH $SERVER_PUBLIC_DIRECTORY_PATH
 
-# Close the connection
 exit
+
 REMOTE_SCRIPT
